95598524f445e8da3e136ddad7421c4ca8ce3194
doxas <eizanco@gmail.com>
2019-08-15 12:08:22 +0900

    add electron builder setting for macos

---
+++ build-mac.js
@@ -0,0 +1,24 @@
+
+const builder = require('electron-builder');
+const fs = require('fs');
+const packageJson = fs.readFileSync('./package.json', 'utf8');
+const parsedPackageJson = JSON.parse(packageJson);
+
+let appName = parsedPackageJson.name;
+parsedPackageJson.main = './server/main.js';
+
+fs.writeFileSync('./app/package.json', JSON.stringify(parsedPackageJson, null, '  '));
+
+builder.build({
+    platform: 'mac',
+    config: {
+        appId: `com.example.${appName}`,
+        mac: {
+            target: 'zip',
+            icon: './dist/icon.png',
+        },
+        directories: {
+            app: './app',
+        },
+    },
+});
